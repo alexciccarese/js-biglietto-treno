bfa983d1d8ef7c18e2e6a5bb9561d7870fa5afb8
alexciccarese <alex.ciccarese@gmail.com>
2025-02-10 17:17:43 +0100

    add until discount

---
+++ main.js
@@ -0,0 +1,34 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
+
+// richiesta Km
+const km = Number(prompt("Quanti chilometri vuoi percorrere?"))
+console.log(km);
+
+
+// Età del passeggero
+const età = Number(prompt("Quanti anni hai?"))
+console.log(età);
+
+// Prezzi
+const prezzoKm = 0.212
+console.log(prezzoKm);
+
+const prezzoTotale = km * prezzoKm
+console.log(prezzoTotale);
+
+// Sconti
+if (età < 18) {
+  prezzoScontato = prezzoTotale * 0.8
+  
+
+} else if (età > 65) {
+  prezzoScontato = prezzoTotale * 0.633
+}
+console.log(prezzoScontato)
+
+
